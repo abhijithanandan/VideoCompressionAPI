3e06e8e1f0388d8a472317b8b23277a43a737338
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2022-08-30 14:53:34 +0530

    Added few more columns to the Video table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, TIMESTAMP, text
 from sqlalchemy.orm import relationship
 
 from .database import Base
@@ -22,3 +22,7 @@ class Video(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     user_id = Column(Integer, ForeignKey("users.id"))
+    title = Column(String, nullable=False)
+    description = Column(String, default=f"Video uploaded for compression", nullable=False)
+    url = Column(String, nullable=False)
+    upload_time = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
