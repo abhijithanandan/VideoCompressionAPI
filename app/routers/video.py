cccfdfb3564afe6fd912e7a45928f2702052e3d6
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2022-08-31 15:50:17 +0530

    Refactored the syntax and returned the user object from verification function

--- app/routers/video.py
+++ app/routers/video.py
@@ -8,7 +8,7 @@ router = APIRouter()
 
 
 @router.get("/videos/{id}")
-def get_all_videos(id: int, db : Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def get_all_videos(id: int, db : Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
 
-    videos = db.query(models.Video).filter(models.Video.user_id == user_id).all()
+    videos = db.query(models.Video).filter(models.Video.user_id == current_user.id).all()
     return {"data": videos}
