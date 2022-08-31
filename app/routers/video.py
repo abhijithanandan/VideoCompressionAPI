034d0adcbe59705c2ccbcb66bdfb10aa95db1524
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2022-08-31 20:09:59 +0530

    Done a correction in video fetch route

--- app/routers/video.py
+++ app/routers/video.py
@@ -7,7 +7,7 @@ from app.database import get_db
 router = APIRouter()
 
 
-@router.get("/videos/{id}")
+@router.get("/videos")
 def get_all_videos(id: int, db : Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
 
     videos = db.query(models.Video).filter(models.Video.user_id == current_user.id).all()
