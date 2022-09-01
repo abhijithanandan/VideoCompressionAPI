35cf3f919989fcc4755446e578fcc37d4651e178
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2022-09-01 10:50:44 +0530

    Moved the database adapter no longer in use to database.py.

--- app/database.py
+++ app/database.py
@@ -1,6 +1,10 @@
+import time
+import psycopg2
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from psycopg2.extras import RealDictCursor
 
 # SQLALCHEMY_DATABASE_URL = 'postgres://<username>:<password>@<ip-address/hostname>/<database-name>'
 SQLALCHEMY_DATABASE_URL = 'postgres://postgres:postgres@localhost/VCApi'
@@ -18,3 +22,20 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+
+"""
+Middleware
+"""
+
+try:
+    conn = psycopg2.connect(host='localhost', database='VCApi', user='postgres', password='postgres',
+                            cursor_factory=RealDictCursor)
+    cursor = conn.cursor()
+    print("Database connection was successful!")
+except Exception as error:
+    print("Connection to database failed")
+    print("Error: ", error)
+    time.sleep(2)
+
--- app/main.py
+++ app/main.py
@@ -1,8 +1,4 @@
-import time
-import psycopg2
-
 from fastapi import FastAPI
-from psycopg2.extras import RealDictCursor
 from app import models
 from app.database import engine
 from app.routers import user, video, auth
@@ -14,20 +10,6 @@ Setup
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-"""
-Middleware
-"""
-
-try:
-    conn = psycopg2.connect(host='localhost', database='VCApi', user='postgres', password='postgres',
-                            cursor_factory=RealDictCursor)
-    cursor = conn.cursor()
-    print("Database connection was successful!")
-except Exception as error:
-    print("Connection to database failed")
-    print("Error: ", error)
-    time.sleep(2)
 
 """
 Routes
