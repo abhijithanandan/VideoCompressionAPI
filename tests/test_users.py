17dacf44f02d071002576a350cbdbfa5213739ec
abhijithanandan <abhijithananthan@gmail.com>
2022-09-04 23:51:56 +0530

    Added the fixtures and create user test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,12 +1,16 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
+from app import schemas
+from .database import client, session
 
 
-def test_root():
+def test_root(client):
     result = client.get("/")
     assert result.json().get('message') == 'Hello World'
     assert result.status_code == 200
 
 
+def test_create_user(client):
+    result = client.post("/users", json={"email": "abhi@vcapi.com", "password": "password"})
+
+    new_user = schemas.UserResponse(**result.json())
+    assert new_user.email == "abhi@vcapi.com"
+    assert result.status_code == 201
